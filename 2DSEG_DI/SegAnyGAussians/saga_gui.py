3ee0e3362bb82220729467bba66a4741216d3c49
eozkzoe <ethanong27@gmail.com>
2025-04-15 02:14:19 +0800

    correct

--- 2DSEG_DI/SegAnyGAussians/saga_gui.py
+++ 2DSEG_DI/SegAnyGAussians/saga_gui.py
@@ -1543,29 +1543,6 @@ class GaussianSplattingGUI:
                         self.engine["scene"].roll_back()
                         self.engine["feature"].roll_back()
 
-            # Apply transformations to all holes after collecting them
-            if holes_info:
-                # Calculate mean center from all holes
-                centers = np.array([np.array(h["center"]) for h in holes_info])
-                mean_center = np.mean(centers, axis=0)
-
-                # Rotation matrix
-                rot_matrix = np.array(
-                    [[0.707, 0, 0.707], [0, 1, 0], [-0.707, 0, 0.707]]
-                )
-
-                # Transform all holes
-                for hole in holes_info:
-                    center = np.array(hole["center"])
-                    normal = np.array(hole["normal"])
-
-                    # Translate to origin, rotate, and translate back
-                    center_centered = center - mean_center
-                    center = (rot_matrix @ center_centered) + mean_center
-                    normal = rot_matrix @ normal
-
-                    hole["center"] = center.tolist()
-                    hole["normal"] = normal.tolist()
             with open(f"./segmentation_res/{mask_name}_holes.json", "w") as f:
                 json.dump({"holes": holes_info}, f, indent=2)
 
